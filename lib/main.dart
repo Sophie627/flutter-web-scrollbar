f0bb9769f21691a96d9e5e9f354ab2eb2656aa75
Sophie627 <alekseizukovski@gmail.com>
2020-08-25 09:31:58 +0300

    Complete bidirection scrollbar for web app

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   //provide the same scrollController for list and draggableScrollbar
-  final ScrollController controller = ScrollController();
-  double _barOffset = 0.0;
+  final ScrollController horizontalController = ScrollController();
+  final ScrollController verticalController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
@@ -20,131 +20,354 @@ class MyApp extends StatelessWidget {
         ),
         //DraggableScrollbar builds Stack with provided Scrollable List of Grid
         body: new DraggableScrollbar(
-//          child: _buildGrid(),
-          heightScrollThumb: 100.0,
-          controller: controller,
+          child: _buildGrid(),
+          horizontalController: horizontalController,
+          verticalController: verticalController,
         ),
+      ),
+    );
+  }
+
+  Widget _buildGrid() {
+    return NotificationListener<ScrollNotification>(
+      onNotification: (ScrollNotification notification) {
+
+      },
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        controller: horizontalController,
+        child: SingleChildScrollView(
+          controller: verticalController,
+          child: Container(
+            width: 2000,
+            height: 2000,
+            child: Image.network('https://repository-images.githubusercontent.com/31792824/fb7e5700-6ccc-11e9-83fe-f602e1e1a9f1',
+              fit: BoxFit.fill,
+            ),
+            color: Colors.green,
+          ),
+        ),//scrollController is final in this stateless widget
       ),
     );
   }
 }
 
 class DraggableScrollbar extends StatefulWidget {
-  final double heightScrollThumb;
+  final double widthHorizontalScrollThumb;
+  final double heightVerticalScrollThumb;
   final Widget child;
-  final ScrollController controller;
+  final ScrollController horizontalController;
+  final ScrollController verticalController;
 
-  DraggableScrollbar({this.heightScrollThumb, this.child, this.controller});
+  DraggableScrollbar({
+    this.widthHorizontalScrollThumb = 100.0,
+    this.heightVerticalScrollThumb = 100.0,
+    this.child,
+    this.horizontalController,
+    this.verticalController,
+  });
 
   @override
   _DraggableScrollbarState createState() => new _DraggableScrollbarState();
 }
 
 class _DraggableScrollbarState extends State<DraggableScrollbar> {
-  //this counts offset for scroll thumb in Vertical axis
-  double _barOffset;
-  //this counts offset for list in Vertical axis
-  double _viewOffset;
+
+  double _horizontalBarOffset;
+  double _verticalBarOffset;
+  double _horizontalViewOffset;
+  double _verticalViewOffset;
+  bool _isHorizontalDragInProcess;
+  bool _isVerticalDragInProcess;
+  bool _isVerticalListener;
+
   @override
   void initState() {
     super.initState();
-    _barOffset = 0.0;
-    _viewOffset = 0.0;
+    _horizontalBarOffset = 0.0;
+    _verticalBarOffset = 0.0;
+    _horizontalViewOffset = 0.0;
+    _verticalViewOffset = 0.0;
+    _isHorizontalDragInProcess = false;
+    _isVerticalDragInProcess = false;
+    _isVerticalListener = false;
   }
 
-  //if list takes 300.0 pixels of height on screen and scrollthumb height is 40.0
-  //then max bar offset is 260.0
-  double get barMaxScrollExtent =>
-      context.size.height - widget.heightScrollThumb;
-  double get barMinScrollExtent => 0.0;
+  double get horizontalBarMaxScrollExtent =>
+      context.size.width - widget.widthHorizontalScrollThumb;
+  double get horizontalBarMinScrollExtent => 0.0;
 
-  //this is usually lenght (in pixels) of list
-  //if list has 1000 items of 100.0 pixels each, maxScrollExtent is 100,000.0 pixels
-  double get viewMaxScrollExtent => widget.controller.position.maxScrollExtent;
-  //this is usually 0.0
-  double get viewMinScrollExtent => widget.controller.position.minScrollExtent;
+  double get verticalBarMaxScrollExtent =>
+      context.size.height - widget.heightVerticalScrollThumb;
+  double get verticalBarMinScrollExtent => 0.0;
 
-  double get scrollbarHeight => barMaxScrollExtent * barMaxScrollExtent / viewMaxScrollExtent;
+  double get horizontalViewMaxScrollExtent => widget.horizontalController.position.maxScrollExtent;
+  double get verticalViewMaxScrollExtent => widget.verticalController.position.maxScrollExtent;
 
-  double getScrollViewDelta(
-      double barDelta,
-      double barMaxScrollExtent,
-      double viewMaxScrollExtent,
-      ) { //propotion
-    return barDelta * viewMaxScrollExtent / barMaxScrollExtent;
+  double get horizontalViewMinScrollExtent => widget.horizontalController.position.minScrollExtent;
+  double get verticalViewMinScrollExtent => widget.verticalController.position.minScrollExtent;
+
+  double getHorizontalScrollViewDelta(
+      double horizontalBarDelta,
+      double horizontalBarMaxScrollExtent,
+      double horizontalViewMaxScrollExtent,
+      ) {//propotion
+    return horizontalBarDelta * horizontalViewMaxScrollExtent / horizontalBarMaxScrollExtent;
+  }
+
+  double getVerticalScrollViewDelta(
+      double verticalBarDelta,
+      double verticalBarMaxScrollExtent,
+      double verticalViewMaxScrollExtent,
+      ) {//propotion
+    return verticalBarDelta * verticalViewMaxScrollExtent / verticalBarMaxScrollExtent;
+  }
+
+  double getHorizontalBarDelta(
+      double horizontalscrollViewDelta,
+      double horizontalBarMaxScrollExtent,
+      double horizontalViewMaxScrollExtent,
+      ) {//propotion
+    return horizontalscrollViewDelta * horizontalBarMaxScrollExtent / horizontalViewMaxScrollExtent;
+  }
+
+  double getVerticalBarDelta(
+      double verticalscrollViewDelta,
+      double verticalBarMaxScrollExtent,
+      double verticalViewMaxScrollExtent,
+      ) {//propotion
+    return verticalscrollViewDelta * verticalBarMaxScrollExtent / verticalViewMaxScrollExtent;
+  }
+
+  void _onHorizontalDragStart(DragStartDetails details) {
+    setState(() {
+      _isHorizontalDragInProcess = true;
+    });
+  }
+
+  void _onVerticalDragStart(DragStartDetails details) {
+    setState(() {
+      _isVerticalDragInProcess = true;
+    });
+  }
+
+  void _onHorizontalDragEnd(DragEndDetails details) {
+    setState(() {
+      _isHorizontalDragInProcess = false;
+    });
+  }
+
+  void _onVerticalDragEnd(DragEndDetails details) {
+    setState(() {
+      _isVerticalDragInProcess = false;
+    });
+  }
+
+  void _onHorizontalDragUpdate(DragUpdateDetails details) {
+    setState(() {
+      _horizontalBarOffset += details.delta.dx;
+
+      if (_horizontalBarOffset < horizontalBarMinScrollExtent) {
+        _horizontalBarOffset = horizontalBarMinScrollExtent;
+      }
+      if (_horizontalBarOffset > horizontalBarMaxScrollExtent) {
+        _horizontalBarOffset = horizontalBarMaxScrollExtent;
+      }
+
+      double horizontalViewDelta = getHorizontalScrollViewDelta(
+          details.delta.dx, horizontalBarMaxScrollExtent, horizontalViewMaxScrollExtent);
+
+      _horizontalViewOffset = widget.horizontalController.position.pixels + horizontalViewDelta;
+      if (_horizontalViewOffset < widget.horizontalController.position.minScrollExtent) {
+        _horizontalViewOffset = widget.horizontalController.position.minScrollExtent;
+      }
+      if (_horizontalViewOffset > horizontalViewMaxScrollExtent) {
+        _horizontalViewOffset = horizontalViewMaxScrollExtent;
+      }
+      widget.horizontalController.jumpTo(_horizontalViewOffset);
+    });
   }
 
   void _onVerticalDragUpdate(DragUpdateDetails details) {
     setState(() {
-      _barOffset += details.delta.dy;
+      _verticalBarOffset += details.delta.dy;
 
-      if (_barOffset < barMinScrollExtent) {
-        _barOffset = barMinScrollExtent;
+      if (_verticalBarOffset < verticalBarMinScrollExtent) {
+        _verticalBarOffset = verticalBarMinScrollExtent;
       }
-      if (_barOffset > barMaxScrollExtent) {
-        _barOffset = barMaxScrollExtent;
+      if (_verticalBarOffset > verticalBarMaxScrollExtent) {
+        _verticalBarOffset = verticalBarMaxScrollExtent;
       }
 
-      double viewDelta = getScrollViewDelta(
-          details.delta.dy, barMaxScrollExtent, viewMaxScrollExtent);
+      double verticalViewDelta = getVerticalScrollViewDelta(
+          details.delta.dy, verticalBarMaxScrollExtent, verticalViewMaxScrollExtent);
 
-      _viewOffset = widget.controller.position.pixels + viewDelta;
-      if (_viewOffset < widget.controller.position.minScrollExtent) {
-        _viewOffset = widget.controller.position.minScrollExtent;
+      _verticalViewOffset = widget.verticalController.position.pixels + verticalViewDelta;
+      if (_verticalViewOffset < widget.verticalController.position.minScrollExtent) {
+        _verticalViewOffset = widget.verticalController.position.minScrollExtent;
       }
-      if (_viewOffset > viewMaxScrollExtent) {
-        _viewOffset = viewMaxScrollExtent;
+      if (_verticalViewOffset > verticalViewMaxScrollExtent) {
+        _verticalViewOffset = verticalViewMaxScrollExtent;
       }
-      widget.controller.jumpTo(_viewOffset);
+      widget.verticalController.jumpTo(_verticalViewOffset);
+    });
+  }
+
+  changeHorizontalPosition(ScrollNotification notification) {
+    print(_isVerticalListener);
+    if (_isHorizontalDragInProcess || _isVerticalDragInProcess || _isVerticalListener) {
+      return;
+    }
+
+    setState(() {
+      if (notification is ScrollUpdateNotification) {
+        _horizontalBarOffset += getHorizontalBarDelta(
+          notification.scrollDelta,
+          horizontalBarMaxScrollExtent,
+          horizontalViewMaxScrollExtent,
+        );
+
+        if (_horizontalBarOffset < horizontalBarMinScrollExtent) {
+          _horizontalBarOffset = horizontalBarMinScrollExtent;
+        }
+        if (_horizontalBarOffset > horizontalBarMaxScrollExtent) {
+          _horizontalBarOffset = horizontalBarMaxScrollExtent;
+        }
+
+        _horizontalViewOffset += notification.scrollDelta;
+        if (_horizontalViewOffset < widget.horizontalController.position.minScrollExtent) {
+          _horizontalViewOffset = widget.horizontalController.position.minScrollExtent;
+        }
+        if (_horizontalViewOffset > horizontalViewMaxScrollExtent) {
+          _horizontalViewOffset = horizontalViewMaxScrollExtent;
+        }
+      }
+    });
+  }
+
+  changeVerticalPosition(ScrollNotification notification) {
+    if (_isVerticalDragInProcess || _isHorizontalDragInProcess) {
+      return;
+    }
+
+    setState(() {
+      if (notification is ScrollStartNotification) {
+        _isVerticalListener = true;
+      }
+      if (notification is ScrollUpdateNotification) {
+        _verticalBarOffset += getVerticalBarDelta(
+          notification.scrollDelta,
+          verticalBarMaxScrollExtent,
+          verticalViewMaxScrollExtent,
+        );
+
+        if (_verticalBarOffset < verticalBarMinScrollExtent) {
+          _verticalBarOffset = verticalBarMinScrollExtent;
+        }
+        if (_verticalBarOffset > verticalBarMaxScrollExtent) {
+          _verticalBarOffset = verticalBarMaxScrollExtent;
+        }
+
+        _verticalViewOffset += notification.scrollDelta;
+        if (_verticalViewOffset < widget.verticalController.position.minScrollExtent) {
+          _verticalViewOffset = widget.verticalController.position.minScrollExtent;
+        }
+        if (_verticalViewOffset > verticalViewMaxScrollExtent) {
+          _verticalViewOffset = verticalViewMaxScrollExtent;
+        }
+      }
+      if (notification is ScrollEndNotification) {
+        _isVerticalListener = false;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return new Stack(children: <Widget>[
-      NotificationListener(
-        onNotification: (t) {
-          if (t is ScrollUpdateNotification) {
-            setState(() {
-              _barOffset = widget.controller.position.pixels / viewMaxScrollExtent * barMaxScrollExtent;
-            });
-          }
+      NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification notification) {
+          changeHorizontalPosition(notification);
         },
-        child: GridView.builder(
-          controller: widget.controller, //scrollController is final in this stateless widget
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 5,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          controller: widget.horizontalController,
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification notification) {
+              changeVerticalPosition(notification);
+            },
+            child: SingleChildScrollView(
+              controller: widget.verticalController,
+              child: Container(
+                width: 2000,
+                height: 2000,
+                child: Image.network('https://repository-images.githubusercontent.com/31792824/fb7e5700-6ccc-11e9-83fe-f602e1e1a9f1',
+                  fit: BoxFit.fill,
+                ),
+                color: Colors.green,
+              ),
+            ),
           ),
-          padding: EdgeInsets.zero,
-          itemCount: 1000,
-          itemBuilder: (context, index) {
-            return Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.all(2.0),
-              color: Colors.grey[300],
-            );
-          },
         ),
       ),
-      Container(
-        color: Colors.white,
-        width: 20.0,
-        height: double.infinity,
-        child: GestureDetector(
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.white,
+            ),
+          ),
+          height: 20.0,
+          width: double.infinity,
+          child: GestureDetector(
+            onHorizontalDragUpdate: _onHorizontalDragUpdate,
+            onHorizontalDragStart: _onHorizontalDragStart,
+            onHorizontalDragEnd: _onHorizontalDragEnd,
+            child: Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: _horizontalBarOffset),
+                child: _buildHorizontalScrollThumb()),
+          ),
+        ),
+      ),
+      Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(
+              color: Colors.white,
+            ),
+          ),
+          width: 20.0,
+          height: double.infinity,
+          child: GestureDetector(
             onVerticalDragUpdate: _onVerticalDragUpdate,
+            onVerticalDragStart: _onVerticalDragStart,
+            onVerticalDragEnd: _onVerticalDragEnd,
             child: Container(
                 alignment: Alignment.topCenter,
-                margin: EdgeInsets.only(top: _barOffset),
-                child: _buildScrollThumb()),
+                margin: EdgeInsets.only(top: _verticalBarOffset),
+                child: _buildVerticalScrollThumb()),
+          ),
         ),
       ),
     ]);
   }
 
-  Widget _buildScrollThumb() {
+  Widget _buildHorizontalScrollThumb() {
     return new Container(
-      height: widget.heightScrollThumb,
-      width: 2.0,
+      width: widget.widthHorizontalScrollThumb,
+      height: 3.0,
+      color: Colors.blue,
+    );
+  }
+  Widget _buildVerticalScrollThumb() {
+    return new Container(
+      height: widget.heightVerticalScrollThumb,
+      width: 3.0,
       color: Colors.blue,
     );
   }
